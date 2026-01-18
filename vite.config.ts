d8f30d4a219e7860021064018c17f89bea77890d
shawnzyp <shawnpeiris22@gmail.com>
2026-01-18 07:13:06 -0500

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// GitHub Pages project site: https://<USER>.github.io/<REPO>/
-// Vite recommends base: '/<REPO>/' for this deployment target.
-export default defineConfig({
-  plugins: [react()],
-  base: '/CCApp/',
-});
