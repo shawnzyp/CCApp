33d23a67aec07393c31fcfcde6668b6d5eff579d
shawnzyp <shawnpeiris22@gmail.com>
2026-01-18 07:13:47 -0500

    Delete src directory

--- src/lib/math.ts
+++
@@ -1,7 +0,0 @@
-export function abilityMod(score: number) {
-  return Math.floor((score - 10) / 2);
-}
-
-export function clamp(n: number, min: number, max: number) {
-  return Math.max(min, Math.min(max, n));
-}
